1bf6e3ee8b108f7e5308484ce0642631cb2ce59b
Alex Puga <alex.puga@wsu.edu>
2020-11-30 15:40:11 -0800

    added file db

--- env/secureUpload/secureUpload/settings.py
+++ env/secureUpload/secureUpload/settings.py
@@ -164,3 +164,5 @@ SOCIAL_AUTH_PIPELINE = (
     'social_core.pipeline.social_auth.load_extra_data',
     'social_core.pipeline.user.user_details',
 )
+
+SOCIAL_AUTH_ADMIN_USER_SEARCH_FIELDS = ['username', 'first_name', 'email', 'uid']
--- env/secureUpload/uploads/models.py
+++ env/secureUpload/uploads/models.py
@@ -3,9 +3,12 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+
 # Create your models here.
 
 class File(models.Model):
     name = models.CharField(max_length=25)
+    extension = models.CharField(max_length=6, default=".")
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
